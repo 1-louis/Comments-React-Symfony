ab5e176dfe0f8f2ba7c62c2f1dc6c6e9e40eee4f
leo <l.louis@outlook.fr>
2021-02-16 04:55:18 +0100

    fin du loding auto

--- assets/js/comment/Comments.jsx
+++ assets/js/comment/Comments.jsx
@@ -3,23 +3,51 @@ import React, {useEffect}from 'react'
 import {usePaginatedFetch} from "./hooks";
 import {Icon} from "../components/Icon";
 
-function Comments() {
 
-    const {item: comments, load, loading, count, hasMore } = usePaginatedFetch('/api/comments')
+const dateFormat= {
+    dateStyle: 'medium',
+    timeStyle: 'short'
+}
+
+function Comments(post, user) {
+
+    const {item: comments, load, loading, count, hasMore } = usePaginatedFetch('/api/comments=' + post )
 
     useEffect(()=>{
         load()
     }, [])
     return <div>
+
         {loading && 'Chargement...'}
-        {JSON.stringify(comments)}
+
         <Title count={count}/>
+        {use && <CommentForm post = {post}/>}
+        {comments.map(c => <Comment key ={c.id} comment={c}/>)}
+
         <button onClick={load}>Chargement des commentaires</button>
         {hasMore && <button disabled={loading} className={'btn btn-primary'}>Plus de commentaire</button> }
     </div>
 
 }
 
+
+const Comment = React.memo( ({comment})=>{
+    const date = new Date(comment.publishedAt)
+    return <div className={"row post-comment"}>
+        <h4 className={"col-sm-3"}>
+            <strong>{comment.author.username}</strong>
+            <strong>{date.toLocaleDateString(undefined, dateFormat)}</strong>
+        </h4>
+        <div className="col-ms-9">
+            <p>{comment.comments}</p>
+        </div>
+    </div>
+})
+
+function CommentForm (post){
+    return <form action="">Article{post}</form>
+
+}
 function Title ({count}){
     return <h3>
         <Icon icon={'comments'}/>
@@ -27,7 +55,10 @@ function Title ({count}){
 }
     class CommentsElement extends HTMLElement {
         connectedCallback() {
-            render(<Comments/>, this)
+            const post  = parseInt(this.dataset.post, 10)
+            const user  = parseInt(this.dataset.user, 10)||null
+
+            render(<Comments post={post} user={user}/>, this)
 
         }
 
